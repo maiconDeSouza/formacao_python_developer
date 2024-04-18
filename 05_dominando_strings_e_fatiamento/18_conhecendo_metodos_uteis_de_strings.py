975e2ea514e701b234b469b349e9c01dce97f55c
Maicon Souza <maicon_souza09@hotmail.com>
2024-04-18 05:23:00 -0300

    18-04-24 05:22

---
+++ 05_dominando_strings_e_fatiamento/18_conhecendo_metodos_uteis_de_strings.py
@@ -0,0 +1,19 @@
+curso = 'cUrso dE pYtHoN'
+
+print('upper ->', curso.upper())
+print('lower ->', curso.lower())
+print('title ->', curso.title())
+print('capitalize ->', curso.capitalize())
+
+print('div'.center(51, '-'))
+
+texto_2 = '  Python  '
+print('strip ->', texto_2.strip())
+print('lstrip ->', texto_2.lstrip())
+print('rstrip ->', texto_2.rstrip())
+
+print('div'.center(51, '-'))
+
+curso_2 = 'Python'
+print('center ->', curso_2.center(10, '#'))
+print('join ->', ','.join(curso_2))
